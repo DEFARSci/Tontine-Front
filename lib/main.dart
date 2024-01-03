0bbc1d7afbe0224430e442814739d08456414e2e
Dammel-Meuz <ndoyemamemoussa@gmail.com>
2024-01-03 13:32:27 +0000

    partie khadre

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,26 @@
 import 'package:flutter/material.dart';
-import 'package:tontinefront/pages/pageapresconnexion.dart';
+import 'home_page.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      title: 'Tontine',
+    return MaterialApp(
+      title: 'Application Gëustu',
       debugShowCheckedModeBanner: false,
-      home: ApresCon(),
+      theme: ThemeData(
+     
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+     
+      home: HomePage(),
     );
   }
 }
